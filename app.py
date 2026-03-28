680d14ce53a5105f4f39baf2e2c5e63c8a516864
Shivashankar <kakanaleshivashankar0@gmail.com>
2026-03-28 16:00:39 +0530

    fix input schema

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from typing import List
 import joblib
 import numpy as np
 
 app = FastAPI()
 
-model = None
+# Load model
+model = joblib.load("model/fraud_model.pkl")
 
-@app.on_event("startup")
-def load_model():
-    global model
-    model = joblib.load("model/fraud_model.pkl")
+# Input schema (IMPORTANT)
+class InputData(BaseModel):
+    data: List[float]
 
+# Home route
 @app.get("/")
 def home():
     return {"message": "Fraud Detection API is running 🚀"}
 
+# Prediction API
 @app.post("/predict")
-def predict(data: List[float]):
-    data = np.array(data).reshape(1, -1)
+def predict(input: InputData):
+    data = np.array(input.data).reshape(1, -1)
+
     prediction = model.predict(data)[0]
 
     if prediction == 1:
         result = "Fraud Transaction 🚨"
     else:
-        result = "Normal Transaction ✅"
+        result = "Normal Transaction 🟩"
 
     return {"prediction": result}
