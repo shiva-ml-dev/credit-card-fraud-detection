f6c659cc302624c02425ba02c4dca22ca5eec6ba
Shivashankar <kakanaleshivashankar0@gmail.com>
2026-03-28 15:42:52 +0530

    fix fastapi docs issue

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI
+from typing import List
 import joblib
 import numpy as np
 
 app = FastAPI()
 
-# model load
-model = joblib.load("model/fraud_model.pkl")
+model = None
+
+@app.on_event("startup")
+def load_model():
+    global model
+    model = joblib.load("model/fraud_model.pkl")
 
 @app.get("/")
 def home():
     return {"message": "Fraud Detection API is running 🚀"}
 
-# NEW API
 @app.post("/predict")
-def predict(data: list):
+def predict(data: List[float]):
     data = np.array(data).reshape(1, -1)
     prediction = model.predict(data)[0]
 
